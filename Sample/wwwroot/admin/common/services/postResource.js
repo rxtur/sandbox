469fc95cfb9af66c3d0e5c87be2196179839f250
rxtur <rtur.net@gmail.com>
2016-05-23 22:01:09 -0500

    RC 2 standard template as starting point

--- Sample/wwwroot/admin/common/services/postResource.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    "use strict";
-
-    angular.module("common.services").factory("postResource", ["$resource", postResource]);
-
-    function postResource($resource) {
-        return $resource("/blog/api/posts/:Id")
-    }
-}());
--- Sample/wwwroot/admin/js/scripts.js
+++
@@ -1,8 +0,0 @@
-$(".nav-sections-title").click(function () {
-    $(".nav-sections-list").slideToggle();
-});
-$(".nav-sections .nav-sections-list .nav-sections-item a").click(function () {
-    $(".nav-sections-list").slideToggle();
-});
-
-// active btn actions
